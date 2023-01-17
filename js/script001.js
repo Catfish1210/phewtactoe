eff02e0c5760a2ea334b1ce40276997a30750ef7
Catfish1210 <chris.laks@gmail.com>
2023-01-17 11:11:22 +0200

    Started over with the js, wanted to tackle this myself

--- js/script001.js
+++ js/script001.js
@@ -1,21 +1,68 @@
-var cells = document.querySelectorAll("td");
-for (var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener("click", function(e) {
-      if (e.target.textContent === "") {
-        var icon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        icon.setAttributeNS(null, "viewBox", "0 0 100 100");
-        icon.setAttributeNS(null, "class", "x-icon");
-        var path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path1.setAttributeNS(null, "d", "M 10 10 L 90 90");
-        path1.setAttributeNS(null, "stroke", "cyan");
-        path1.setAttributeNS(null, "stroke-width", "20");
-        var path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path2.setAttributeNS(null, "d", "M 10 90 L 90 10");
-        path2.setAttributeNS(null, "stroke", "cyan");
-        path2.setAttributeNS(null, "stroke-width", "20");
-        icon.appendChild(path1);
-        icon.appendChild(path2);
-        e.target.appendChild(icon);
-      }
-    });
-}
+// create an SVG element X
+var xSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+xSVG.setAttribute("width", "50");
+xSVG.setAttribute("height", "50");
+// create the X shape
+var xLine1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+xLine1.setAttribute("x1", "10");
+xLine1.setAttribute("y1", "10");
+xLine1.setAttribute("x2", "40");
+xLine1.setAttribute("y2", "40");
+xLine1.setAttribute("stroke", "red");
+xLine1.setAttribute("stroke-width", "4");
+xSVG.appendChild(xLine1); 
+var xLine2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+xLine2.setAttribute("x1", "40");
+xLine2.setAttribute("y1", "10");
+xLine2.setAttribute("x2", "10");
+xLine2.setAttribute("y2", "40");
+xLine2.setAttribute("stroke", "red");
+xLine2.setAttribute("stroke-width", "4");
+xSVG.appendChild(xLine2);
+
+// create an SVG element O
+var oSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+oSVG.setAttribute("width", "50");
+oSVG.setAttribute("height", "50");
+
+var outerCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+outerCircle.setAttribute("cx", "25");
+outerCircle.setAttribute("cy", "25");
+outerCircle.setAttribute("r", "20");
+outerCircle.setAttribute("stroke", "#f1b142");
+outerCircle.setAttribute("stroke-width", "4");
+outerCircle.setAttribute("fill", "none");
+oSVG.appendChild(outerCircle);
+
+var innerCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+innerCircle.setAttribute("cx", "25");
+innerCircle.setAttribute("cy", "25");
+innerCircle.setAttribute("r", "15");
+innerCircle.setAttribute("fill", "#203540");
+oSVG.appendChild(innerCircle);
+
+
+
+var cell0 = document.getElementById("cell-0")
+cell0.addEventListener("click", function() {
+    // append the SVG to the button
+    cell0.appendChild(xSVG);
+});
+
+var cell1 = document.getElementById("cell-1")
+cell1.addEventListener("click", function() {
+    cell1.appendChild(oSVG);
+    alert("Cell 2 was clicked!");
+});
+
+var cell2 = document.getElementById("cell-2")
+cell2.addEventListener("click", function() {
+    alert("Cell 3 was clicked!");
+});
+
+var restartBtn = document.getElementById("restartBtn")
+restartBtn.addEventListener("click", function() {
+        cell0.removeChild(xSVG);
+
+});
+
