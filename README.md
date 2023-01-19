d3d33c713b1341564edbb4ad4e937099a7551115
Catfish1210 <chris.laks@gmail.com>
2023-01-19 20:00:29 +0200

    Tried/started a different approach with functions, will look into it further

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # phewtactoe
 Browser based TicTacToe game with HTML, CSS, JS
-Testing pull
+
+Here's a more detailed explanation of the logic behind a Tic Tac Toe game:
+
+    Initialize the game state variables:
+        board is an array that keeps track of the state of each cell on the game board. Empty cells are represented as empty strings, "X" represents a cell that has been marked by player 1, and "O" represents a cell that has been marked by player 2.
+        players is an array that holds the symbols used by each player. In this case, it is an array of two strings, "X" and "O".
+        currentPlayer is a variable that keeps track of which player's turn it is. It starts at 0, which represents player 1.
+
+    Create a render() function that updates the game board to reflect the current state of the board array. It loops through the board array and updates the HTML of each cell to match the corresponding element in the array.
+
+    Create a cellClick() function that is called when a cell is clicked. It takes the event object as a parameter. The function gets the cell that was clicked and its index in the board array. It then updates the board array with the symbol of the current player and changes the value of currentPlayer to switch to the other player's turn.
+
+    Add an event listener to each cell that calls the cellClick() function when clicked.
+
+    Call the render() function to initialize the game board.
+
+    Add the logic to check for win, tie, and resetting the game.
+        To check for a win, you can check the rows, columns and diagonals of the board if they contain the same symbol and not a empty string.
+        To check for a tie you can check if the board have no empty cell remaining and no one have won.
+        To reset the game you can simply clear the board array and set the currentPlayer to 0 and call the render function again.
+
+It's also a good idea to handle cases such as clicking a cell that has already been marked and invalid moves.
+
+This is a simple explanation of a Tic Tac Toe game logic, you can also add more advanced features like AI player, difficulty levels and more.
--- js/script001.js
+++ js/script001.js
@@ -1,24 +1,4 @@
-// create an SVG element X
-var xSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-xSVG.setAttribute("width", "50");
-xSVG.setAttribute("height", "50");
-// create the X shape
-var xLine1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-xLine1.setAttribute("x1", "10");
-xLine1.setAttribute("y1", "10");
-xLine1.setAttribute("x2", "40");
-xLine1.setAttribute("y2", "40");
-xLine1.setAttribute("stroke", "red");
-xLine1.setAttribute("stroke-width", "4");
-xSVG.appendChild(xLine1); 
-var xLine2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-xLine2.setAttribute("x1", "40");
-xLine2.setAttribute("y1", "10");
-xLine2.setAttribute("x2", "10");
-xLine2.setAttribute("y2", "40");
-xLine2.setAttribute("stroke", "red");
-xLine2.setAttribute("stroke-width", "4");
-xSVG.appendChild(xLine2);
+
 
 // create an SVG element O
 var oSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -40,9 +20,22 @@ innerCircle.setAttribute("cy", "25");
 innerCircle.setAttribute("r", "15");
 innerCircle.setAttribute("fill", "#203540");
 oSVG.appendChild(innerCircle);
+let oSVG1 = oSVG;
+
+
+//Display possible SVGs (in the future maybe highlight the one whos turn it is.)
+var playerSVGs = document.getElementById("SVGS")
+playerSVGs.append(xSVG);
+playerSVGs.append(oSVG1);
 
 
 
+
+
+
+
+
+// Buttons
 var cell0 = document.getElementById("cell-0")
 cell0.addEventListener("click", function() {
     // append the SVG to the button
@@ -52,7 +45,6 @@ cell0.addEventListener("click", function() {
 var cell1 = document.getElementById("cell-1")
 cell1.addEventListener("click", function() {
     cell1.appendChild(oSVG);
-    alert("Cell 2 was clicked!");
 });
 
 var cell2 = document.getElementById("cell-2")
@@ -62,7 +54,95 @@ cell2.addEventListener("click", function() {
 
 var restartBtn = document.getElementById("restartBtn")
 restartBtn.addEventListener("click", function() {
-        cell0.removeChild(xSVG);
 
+        // Should add all cell removeChild's here (xSVG) and (oSVG)
 });
 
+function createX() {
+    var xSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    xSVG.setAttribute("width", "50");
+    xSVG.setAttribute("height", "50");
+    
+    var xLine1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    xLine1.setAttribute("x1", "10");
+    xLine1.setAttribute("y1", "10");
+    xLine1.setAttribute("x2", "40");
+    xLine1.setAttribute("y2", "40");
+    xLine1.setAttribute("stroke", "#38c4bd");
+    xLine1.setAttribute("stroke-width", "4");
+    xSVG.appendChild(xLine1); 
+    var xLine2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    xLine2.setAttribute("x1", "40");
+    xLine2.setAttribute("y1", "10");
+    xLine2.setAttribute("x2", "10");
+    xLine2.setAttribute("y2", "40");
+    xLine2.setAttribute("stroke", "#38c4bd");
+    xLine2.setAttribute("stroke-width", "4");
+    xSVG.appendChild(xLine2);
+}
+
+function createO() {
+
+}
+
+
+function cellCheck() {
+    // Inputs : Who's turn is it (x or o) & what cell was clicked
+    // Check if cell is empty
+    // D
+}
+
+
+
+
+function cellListener() {
+
+    var cell0 = document.getElementById("cell-0")
+    cell0.addEventListener("click", function() {
+    // what do after click
+});
+
+    var cell1 = document.getElementById("cell-1")
+    cell1.addEventListener("click", function() {
+    // what do after click
+});
+
+    var cell2 = document.getElementById("cell-2")
+    cell2.addEventListener("click", function() {
+    // what do after click
+}); 
+
+    var cell3 = document.getElementById("cell-3")
+    cell3.addEventListener("click", function() {
+    // what do after click
+    createX();
+    cell3.appendChild(xSVG);
+});
+
+    var cell4 = document.getElementById("cell-4")
+    cell4.addEventListener("click", function() {
+    // what do after click
+});
+
+    var cell5 = document.getElementById("cell-5")
+    cell5.addEventListener("click", function() {
+    // what do after click
+});
+
+    var cell6 = document.getElementById("cell-6")
+    cell6.addEventListener("click", function() {
+    // what do after click
+});
+
+    var cell7 = document.getElementById("cell-7")
+    cell7.addEventListener("click", function() {
+    // what do after click
+});
+    var cell8 = document.getElementById("cell-8")
+    cell8.addEventListener("click", function() {
+    // what do after click
+});
+
+}
+
+cellListener();
